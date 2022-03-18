97ee1f9f15066c91dd21ac86d2ca4858e106f76b
Saito Koki <saito.koki@kyla.co.jp>
2022-03-18 23:39:35 +0900

    add base

---
+++ fmt2_to_vtk_legacy_converter.py
@@ -0,0 +1,7 @@
+from base_vtk_legacy_converter import BaseVtkLegacyConverter
+
+
+class Fmt2ToVtkLegacyConverter(BaseVtkLegacyConverter):
+    def ReadFile(self, in_file_name):
+        """Need override."""
+        pass
