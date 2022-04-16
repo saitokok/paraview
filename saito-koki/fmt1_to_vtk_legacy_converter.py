ccfb39cd1bd1ed7d077c87039dda3e564dcebf5c
Saito Koki <saito.koki@kyla.co.jp>
2022-04-16 23:42:22 +0900

    add saito pr

--- saito-koki/fmt1_to_vtk_legacy_converter.py
+++ saito-koki/fmt1_to_vtk_legacy_converter.py
@@ -1,7 +1,38 @@
 from base_vtk_legacy_converter import BaseVtkLegacyConverter
 
 
-class Fmt1ToVtkLegacyConverter(BaseVtkLegacyConverter):
+class HogeToVtkLegacyConverter(BaseVtkLegacyConverter):
     def ReadFile(self, in_file_name):
         """Need override."""
-        pass
+        with open(in_file_name) as f:
+            lines = f.read().splitlines()
+        self.object_3d.points = self.create_points(lines)
+        self.object_3d.cells = self.create_cells(lines)
+        self.object_3d.cell_types = ["tri" for _ in self.object_3d.cells]
+
+    def create_points(self, lines):
+        return [self.convert_xyz(l) for l in lines[3:self.get_cells_index(lines) - 1]]
+
+    def create_cells(self, lines):
+        return [self.convert_cells(l) for l in lines[self.get_cells_index(lines) + 3:-1]]
+
+    @staticmethod
+    def convert_cells(l):
+        _, _, p1, p2, p3 = l.split(",")
+        return [p1, p2, p3]
+
+    @staticmethod
+    def convert_xyz(l):
+        _, x, y, z = l.split(",")
+        return [x, y, z]
+
+    @staticmethod
+    def get_cells_index(lines):
+        for i, line in enumerate(lines):
+            if line == "[cells]":
+                return i
+        return -1
+
+
+if __name__ == '__main__':
+    HogeToVtkLegacyConverter().Execute("teapot.fmt1", "output.vtk")
