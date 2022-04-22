2b001dd49774bc8bb023c9bf7f1304ff276575bf
iChiba <ichiba_k@kezukaya.com>
2022-04-22 17:44:12 +0900

    インスタンス作成タイミングを調整

--- kozuka-toma/converter_kozuka.py
+++ kozuka-toma/converter_kozuka.py
@@ -2,12 +2,12 @@ from fmt1_to_vtk_legacy_converter import Fmt1ToVtkLegacyConverter
 from fmt2_to_vtk_legacy_converter import Fmt2ToVtkLegacyConverter
 
 file_name = input("Enter target file name in data directory (fmt1|fmt2): ")
-Fmt1Converter = Fmt1ToVtkLegacyConverter()
-Fmt2Converter = Fmt2ToVtkLegacyConverter()
 
 if ".fmt1" in file_name:
+    Fmt1Converter = Fmt1ToVtkLegacyConverter()
     Fmt1Converter.Execute("./data/" + file_name, "./output/" + file_name + ".vtk")
 elif ".fmt2" in file_name:
+    Fmt2Converter = Fmt2ToVtkLegacyConverter()
     Fmt2Converter.Execute("./data/" + file_name, "./output/" + file_name + ".vtk")
 else:
     print("ERROR: allowed file type is only (.fmt1|.fmt2) ")
