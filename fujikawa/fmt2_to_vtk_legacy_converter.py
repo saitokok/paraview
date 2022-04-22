b613e66f95f035c0ce77bf7520f7393ef36d7cf5
sho <sho@example.com>
2022-04-22 15:25:04 +0900

    fix

--- fujikawa/fmt2_to_vtk_legacy_converter.py
+++ fujikawa/fmt2_to_vtk_legacy_converter.py
@@ -7,9 +7,7 @@ class Fmt2ToVtkLegacyConverter(BaseVtkLegacyConverter):
 
         with open(in_file_name, mode="r") as file_obj:
 
-            point_flg = 1
-            cells_flg = 0
-            type_flg = 0
+            type_cd = 0
             skip_counter = 0
 
             while True:
@@ -19,12 +17,10 @@ class Fmt2ToVtkLegacyConverter(BaseVtkLegacyConverter):
                     break
 
                 if line.find('cells') > 0:
-                    cells_flg = 1
-                    point_flg = 0
+                    type_cd = 1
                     skip_counter = 0
                 elif line.find('cell_types') > 0:
-                    type_flg = 1
-                    cells_flg = 0
+                    type_cd = 2
                     skip_counter = 0
 
                 if skip_counter != 3:
@@ -34,16 +30,16 @@ class Fmt2ToVtkLegacyConverter(BaseVtkLegacyConverter):
                 if line.find(',') > 0:
                     line_splits = line.split('(')[1].replace(')', '')
                     line_splits_2 = line_splits.split(',')
-                elif type_flg == 1:
+                elif type_cd == 2:
                     line_type = line.replace('\n', '')
                     self.object_3d.cell_types.append(line_type)
                     continue
                 else:
                     continue
 
-                if point_flg == 1:
+                if type_cd == 0:
                     self.object_3d.points.append([line_splits_2[0], line_splits_2[1], line_splits_2[2]])
-                elif cells_flg == 1:
+                elif type_cd == 1:
                     self.object_3d.cells.append([line_splits_2[0], line_splits_2[1], line_splits_2[2]])
 
         pass
